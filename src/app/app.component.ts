6932aa906ffbfa10408f65d717152a59f92bd1af
takuya-nakayasu <l08084.1989@gmail.com>
2021-09-24 15:22:54 +0900

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { BasicService } from './services/basic.service';
+import { SampleService } from './services/sample.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-sample-work';
+  constructor(
+    private sampleService: SampleService,
+    private basicService: BasicService
+  ) {}
 }
